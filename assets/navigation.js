3c86848e4df7cb642f67ddce5447b05f34e9936f
brandonlehmann <brandonlehmann@users.noreply.github.com>
2024-01-27 14:19:57 +0000

    Build: (dbf7e67) Update dependencies

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKSU1LLM0pUdJRKkgsyVCyUsrNTynNSS3WyyjJzVFGyGZn5qUoWZkYWpoYG5jU6sD1h2TmphYhdCfnJBYXpxbrg4XBZsD1GhpZ1MYCABAxqLx5AAAA"
