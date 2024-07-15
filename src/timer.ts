dcb6091d58492ee391f4d225738f2f3d249df6c6
Brandon Lehmann <brandonlehmann@gmail.com>
2024-07-15 13:41:57 -0400

    - Catch errors when a 'tick' handler throws them - Added AsyncTimer class for performing async methods automatically on a regular timer - Added SyncTimer class for performing sync methods automatically on a regular basis - Both AsyncTimer & SyncTimer swallow errors by default, to get the errors, .on('error') - Fixed an issue whereby the Timer was blocking the thread preventing it from dying if it had not been started (note: you must destroy it once started to 'kill' it)

--- src/timer.ts
+++ src/timer.ts
@@ -20,9 +20,10 @@
 
 import { EventEmitter } from 'events';
 
-export default class Timer extends EventEmitter {
+export default class Timer<Type = any> extends EventEmitter {
     public paused = true;
     private _timer?: NodeJS.Timeout;
+    private readonly _args: any[];
 
     /**
      * Constructs a new instance of the timer
@@ -38,20 +39,11 @@ export default class Timer extends EventEmitter {
     constructor (public interval: number, autoStart = false, ...args: any[]) {
         super();
 
+        this._args = args;
+
         if (autoStart) {
-            this.paused = false;
+            this.start();
         }
-
-        const tick = (): void => {
-            if (!this.paused) {
-                this.tick(...args);
-            }
-            if (!this._destroyed) {
-                this._timer = setTimeout(tick, this.interval);
-            }
-        };
-
-        tick();
     }
 
     private _destroyed = false;
@@ -68,14 +60,56 @@ export default class Timer extends EventEmitter {
      * @param event
      * @param listener
      */
-    public on(
-        event: 'tick', listener: (...args: any[]) => void): this;
+    public on(event: 'tick', listener: (...args: any[]) => void): this;
+
+    /**
+     * Event that is emitted when we have an error
+     *
+     * @param event
+     * @param listener
+     */
+    public on(event: 'error', listener: (error: Error) => void): this;
+
+    /**
+     * Event that is emitted when we start the timer
+     *
+     * @param event
+     * @param listener
+     */
+    public on(event: 'start', listener: () => void): this;
+
+    /**
+     * Event that is emitted when we stop the timer
+     *
+     * @param event
+     * @param listener
+     */
+    public on(event: 'stop', listener: () => void): this;
+
+    /** @ignore */
+    public on(event: 'data', listener: (payload: Type, timestamp: number, interval: number) => void): this;
 
     /** @ignore */
     public on (
         event: any, listener: (...args: any[]) => void): this {
         return super.on(event, listener);
     }
+
+    /**
+     * Event that is emitted only once when we start the timer
+     *
+     * @param event
+     * @param listener
+     */
+    public once(event: 'start', listener: () => void): this;
+
+    /**
+     * Event that is emitted only once when we stop the timer
+     *
+     * @param event
+     * @param listener
+     */
+    public once(event: 'stop', listener: () => void): this;
 
     /**
      * Event that is emitted only once when a tick occurs
@@ -86,15 +120,71 @@ export default class Timer extends EventEmitter {
     public once(event: 'tick', listener: (...args: any[]) => void): this;
 
     /** @ignore */
+    public once(event: 'data', listener: (payload: Type, timestamp: number, interval: number) => void): this;
+
+    /**
+     * Event that is emitted only once when we have an error
+     *
+     * @param event
+     * @param listener
+     */
+    public once(event: 'error', listener: (error: Error) => void): this;
+
+    /** @ignore */
     public once (
         event: any, listener: (...args: any[]) => void): this {
         return super.on(event, listener);
     }
 
     /**
+     * Event that is emitted only once when we start the timer
+     *
+     * @param event
+     * @param listener
+     */
+    public off(event: 'start', listener: () => void): this;
+
+    /**
+     * Event that is emitted only once when we stop the timer
+     *
+     * @param event
+     * @param listener
+     */
+    public off(event: 'stop', listener: () => void): this;
+
+    /**
+     * Event that is emitted only once when a tick occurs
+     *
+     * @param event
+     * @param listener
+     */
+    public off(event: 'tick', listener: (...args: any[]) => void): this;
+
+    /** @ignore */
+    public off(event: 'data', listener: (payload: Type, timestamp: number, interval: number) => void): this;
+
+    /**
+     * Event that is emitted only once when we have an error
+     *
+     * @param event
+     * @param listener
+     */
+    public off(event: 'error', listener: (error: Error) => void): this;
+
+    /** @ignore */
+    public off (
+        event: any, listener: (...args: any[]) => void): this {
+        return super.on(event, listener);
+    }
+
+    /**
      * Destroys the timer
+     *
+     * Note: This is destructive, once destroyed, it cannot be restarted
      */
     public destroy (): void {
+        this.stop();
+
         this._destroyed = true;
 
         if (this._timer) {
@@ -108,14 +198,24 @@ export default class Timer extends EventEmitter {
      * Starts the timer
      */
     public start (): void {
-        this.paused = false;
+        if (this.paused && !this._destroyed) {
+            this.paused = false;
+
+            this._tick();
+
+            this.emit('start');
+        }
     }
 
     /**
      * Stops the timer
      */
     public stop (): void {
-        this.paused = true;
+        if (!this.paused && !this.destroyed) {
+            this.paused = true;
+
+            this.emit('stop');
+        }
     }
 
     /**
@@ -124,16 +224,95 @@ export default class Timer extends EventEmitter {
      * @param args
      */
     public tick (...args: any[]): void {
+        if (this.destroyed) return;
+
         this.emit('tick', ...args);
     }
 
     /**
      * Toggles the timer on/off
+     *
+     * @returns boolean whether the timer is running or not
      */
     public toggle (): boolean {
-        this.paused = (!this.paused);
+        if (this.destroyed) return false;
 
-        return this.paused;
+        if (this.paused) {
+            this.start();
+        } else {
+            this.stop();
+        }
+
+        return !this.paused;
+    }
+
+    private _tick = (): void => {
+        if (!this.paused) {
+            try {
+                this.tick(...this._args);
+            } catch (error: any) {
+                this.emit('error', error instanceof Error ? error : new Error(error.toString()));
+            }
+        }
+        if (!this._destroyed) {
+            this._timer = setTimeout(this._tick, this.interval);
+        }
+    };
+}
+
+/**
+ * A helper class that performs a sync function at the given interval
+ * and emits the result on a regular basis
+ */
+export class SyncTimer<Type = any> extends Timer<Type> {
+    public constructor (func: () => Type, interval: number, autostart = false) {
+        super(interval);
+
+        this.on('tick', () => {
+            try {
+                const now = Math.round((new Date()).getTime() / 1000);
+
+                // go get the results
+                const result = func();
+
+                // emit the results
+                this.emit('data', result, now, interval);
+            } catch (error: any) {
+                this.emit('error', error instanceof Error ? error : new Error(error.toString()));
+            }
+        });
+
+        this.on('error', () => {}); // swallow the error by default
+
+        if (autostart) this.start();
+    }
+}
+
+/**
+ * A helper class that performs an async function at the given interval
+ * and emits the result on a regular basis
+ */
+export class AsyncTimer<Type = any> extends Timer<Type> {
+    public constructor (func: () => Promise<Type>, interval: number, autostart = false) {
+        super(interval);
+
+        this.on('tick', async () => {
+            try {
+                const now = Math.round((new Date()).getTime() / 1000);
+
+                // go get the results
+                const result = await func();
+
+                // emit the results
+                this.emit('data', result, now, interval);
+            } catch (error: any) {
+                this.emit('error', error instanceof Error ? error : new Error(error.toString()));
+            }
+        });
+
+        this.on('error', () => {}); // swallow the error by default
+
+        if (autostart) this.start();
     }
 }
 
